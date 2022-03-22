6e816febd1e1be87f8f8e294c32fd6a9f2c1f627
TheDoctorWhom <Zak.dutton@gmail.com>
2022-03-22 17:08:54 -0600

    error correction in resume.jsx

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -3,15 +3,6 @@ import React, { useEffect, useState } from 'react';
 import Download from './zak_2022_resume.pdf';
 
 const Resume = () => {
-  const [imageUrl, setImageUrl] = useState('');
-
-  // useEffect(() => {
-  //   fetch('https://dog.ceo/api/breeds/image/random')
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setImageUrl(data.message);
-  //     });
-  // }, []);
 
   return (
     <div>
